689f64cb93e856d1db71e46562bfcd81380a5227
sarthakkshirsagar <sarthakkshirsager436@gmail.com>
2026-08-03 14:13:49 +0530

    Use Render PORT

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const authRoutes = require("./routes/authRoutes");
-const shops=require("./routes/shops");
-const bookings=require("./routes/bookingRoute");
 
+const authRoutes = require("./routes/authRoutes");
+const shops = require("./routes/shops");
+const bookings = require("./routes/bookingRoute");
 
 const connectDB = require("./config/db");
 
 const app = express();
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 
 connectDB();
@@ -20,9 +20,12 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/shops",shops);
-app.use("/api/available",bookings);
+app.use("/api/shops", shops);
+app.use("/api/available", bookings);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
